292b90ac1e47dbdeddb618a495468209b8431e3a
Jason Harris <lord.jtharris@gmail.com>
2023-05-17 10:06:15 -0500

    The moose has a response to the intial question.

--- Program.cs
+++ Program.cs
@@ -53,4 +53,10 @@ MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic!");
 
 bool isTrue = MooseAsks("Is Canada Real?");
-Console.WriteLine(isTrue);
+
+if(isTrue){
+    Console.WriteLine("Really? It seems unlikely");
+}
+else{
+    Console.WriteLine("I   K N E W   I T !");
+}
